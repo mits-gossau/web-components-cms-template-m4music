c0a7ed930b6f782ddd200b766aa8ce7b6bba8e28
ed <88322685+edmgb@users.noreply.github.com>
2021-10-05 11:09:22 +0200

    linter and css docu

--- src/es/components/organisms/Footer.js
+++ src/es/components/organisms/Footer.js
@@ -13,12 +13,44 @@ import BaseFooter from '../web-components-cms-template/src/es/components/organis
  * @type {CustomElementConstructor}
  * @attribute {}
  * @css {
- *  NOTE: grid-area: footer;
- *  var(--background-color, black)
+ * NOTE: grid-area: footer;
+ * var(--background-color, black)
+ * var(--z-index, 100)
+ * var(--content-spacing, 0) auto
+ * var(--content-width, 80%)
+ * var(--align-content, normal)
+ * var(--border-top, 0)
+ * var(--box-sizing, content-box)
+ * var(--color, white)
+ * var(--display, flex)
+ * var(--flex-direction, row)
+ * var(--justify-content, normal)
+ * var(--padding, 0)
+ * var(--ul-list-style-type, none)
+ * var(--ul-margin, 0)
+ * var(--ul-padding, 0)
+ * var(--li-padding, 0)
+ * var(--meta-font-size, 1em)
+ * var(--meta-padding, 0)
+ * var(--engagement-text-max-width, auto)
+ * var(--ul-li-padding, 0)
+ * var(--div-padding, 0)
+ * var(--a-color-hover, #FFFFFF)
+ * var(--a-link-display-mobile)
+ * var(--a-link-display)
+ * var(--a-link-content-spacing, 0)
+ * var(--content-spacing-mobile, 0) auto;
+ * var(--content-width-mobile, 80%)
+ * var(--flex-direction-mobile, column)
+ * var(--ul-margin-mobile, 0.5em 0)
+ * var(--engagement-padding-mobile, 0)
+ * var(--meta-font-size-mobile, 1em)
+ * var(--a-font-size-mobile, 1em)
+ * var(--div-padding-mobile, 0)
  * }
  */
 export default class Footer extends BaseFooter {
-  connectedCallback() {
+  connectedCallback () {
     if (this.shouldComponentRenderCSS()) this.renderCSS()
     this.renderHTML()
   }
@@ -28,7 +60,7 @@ export default class Footer extends BaseFooter {
    *
    * @return {void}
    */
-  renderCSS() {
+  renderCSS () {
     this.css = /* css */`
     
       :host {
@@ -43,8 +75,8 @@ export default class Footer extends BaseFooter {
       }
       
       :host > footer {
-        border-top: var(--border-top, 0);
         align-content: var(--align-content, normal);
+        border-top: var(--border-top, 0);
         box-sizing: var(--box-sizing, content-box);
         color: var(--color, white);
         display: var(--display, flex);
@@ -55,13 +87,13 @@ export default class Footer extends BaseFooter {
 
       :host > footer ul {
         list-style-type: var(--ul-list-style-type, none);
-        padding: var(--ul-padding, 0);
         margin: var(--ul-margin, 0);
+        padding: var(--ul-padding, 0);
       }
 
       :host > footer li {
-        padding:var(--li-padding, 0);
         float: left;
+        padding:var(--li-padding, 0);
       }
 
       :host .footer__main, .footer__meta {
@@ -95,18 +127,19 @@ export default class Footer extends BaseFooter {
       }
 
       :host a-link {
-        --padding: var(--a-link-content-spacing, 0);
-        --display: var(--a-link-display);
-        --display-mobile: var(--a-link-display-mobile);
-        --font-size:var(--a-font-size, 1em);
         --color-hover: var(--a-color-hover, #FFFFFF);
+        --display-mobile: var(--a-link-display-mobile);
+        --display: var(--a-link-display);
+        --font-size:var(--a-font-size, 1em);
+        --font-size-mobile: var(--a-font-size-mobile, 1em);
+        --padding: var(--a-link-content-spacing, 0);
       }
 
       @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
       
         :host > * {
-          width: var(--content-width-mobile, 80%);
           margin: var(--content-spacing-mobile, 0) auto; 
+          width: var(--content-width-mobile, 80%);
         }
         :host > footer {     
           flex-direction: var(--flex-direction-mobile, column);
