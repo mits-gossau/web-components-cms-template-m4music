0a46fc6f0edb6e3f2275d13fdca91cbac72772cc
ed <88322685+edmgb@users.noreply.github.com>
2021-10-08 14:00:24 +0200

    fix: footer social media icons

--- src/es/components/organisms/Footer.js
+++ src/es/components/organisms/Footer.js
@@ -35,6 +35,10 @@ import BaseFooter from '../web-components-cms-template/src/es/components/organis
  * var(--engagement-text-max-width, auto)
  * var(--flex-direction-mobile, column)
  * var(--flex-direction, row)
+ * var(--icons-div-padding-mobile, 1em)
+ * var(--icons-div-padding, 1em)
+ * var(--icons-img-height, 1em)
+ * var(--icons-img-width, 1em)
  * var(--justify-content, normal)
  * var(--li-padding, 0)
  * var(--meta-font-size-mobile, 1em)
@@ -62,18 +66,15 @@ export default class Footer extends BaseFooter {
    */
   renderCSS() {
     this.css = /* css */`
-    
       :host {
         background-color: var(--background-color, black);
         grid-area: footer;
         z-index: var(--z-index, 100);
       }
-
       :host > * {
         margin: var(--content-spacing, 0) auto;
         width: var(--content-width, 80%);
       }
-      
       :host > footer {
         align-content: var(--align-content, normal);
         border-top: var(--border-top, 0);
@@ -84,59 +85,59 @@ export default class Footer extends BaseFooter {
         justify-content: var(--justify-content, normal);
         padding: var(--padding, 0);
       }
-
       :host > footer ul {
         list-style-type: var(--ul-list-style-type, none);
         margin: var(--ul-margin, 0);
         padding: var(--ul-padding, 0);
       }
-
       :host > footer li {
         float: left;
         padding:var(--li-padding, 0);
       }
-
       :host .footer__main, .footer__meta {
         display:flex;
         flex-direction: row;
         justify-content: space-between;
       }
-
       :host .footer__meta {
         font-size:var(--meta-font-size, 1em);
         padding:var(--meta-padding, 0);
       }
-
       :host .footer__engagement {
         max-width:var(--engagement-text-max-width, auto);
       }
-
       :host .footer__nav {
         display:flex;
         flex-direction: row;
         flex-wrap: wrap;
       }
-
       :host .footer__nav ul > li {
         float: initial;
         padding: var(--ul-li-padding, 0);
       }
-
       :host .footer__nav > div {
         padding: var(--div-padding, 0);
       }
-
+      :host .icons {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+      }
+      :host .icons div {
+        padding:var(--icons-div-padding, 1em);
+      }
+      :host .icons div > img {
+        width: var(--icons-img-width, 1em);
+        height: var(--icons-img-height, 1em);
+      }
       :host a-link {
         --color-hover: var(--a-color-hover, #FFFFFF);
         --display-mobile: var(--a-link-display-mobile);
         --display: var(--a-link-display);
         --font-size:var(--a-font-size, 1em);
-        /*--font-size-mobile: var(--a-font-size-mobile, 1em);*/
         --padding: var(--a-link-content-spacing, 0);
       }
-
-      @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
-      
+      @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {  
         :host > * {
           margin: var(--content-spacing-mobile, 0) auto; 
           width: var(--content-width-mobile, 80%);
@@ -147,31 +148,33 @@ export default class Footer extends BaseFooter {
         :host > footer ul {
           margin: var(--ul-margin-mobile, 0.5em 0);
         }
-
         :host .footer__main, .footer__meta {
           flex-direction: column;
         }
-
         :host .footer__engagement {
           padding: var(--engagement-padding-mobile, 0);
         }
-
         :host .footer__nav {
           flex-direction: column;
         }
-
         :host .footer__meta {
           font-size:var(--meta-font-size-mobile, 1em);
         }
-
         :host a-link {
           --font-size: var(--a-font-size-mobile, 1em);
         }
-
         :host .footer__nav > div {
           padding: var(--div-padding-mobile, 0);
         }
-        
+        :host .icons {
+          display: flex;
+          flex-direction: row;
+          justify-content: flex-start;
+          align-items: center;
+        }
+        :host .icons div {
+          padding:var(--icons-div-padding-mobile, 1em);
+        }
       }
     `
   }
