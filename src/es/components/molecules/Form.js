ffc4c7f17e8cb0b01a7fe7361546f3b76857c581
joeljxse <joeljosewattwil@gmail.com>
2021-11-02 15:34:39 +0100

    styling form-caption

--- src/es/components/molecules/Form.js
+++ src/es/components/molecules/Form.js
@@ -19,6 +19,18 @@ export default class Form extends BaseForm {
       :host fieldset {
         border: var(--fieldset-border, none);
       }
+
+      :host h4.form-caption {
+        font-size: var(--h4-font-size, 1.25rem) !important;
+        line-height: var(--h4-line-height, 125%) !important;
+      }
+
+      @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
+        :host h4.form-caption {
+          font-size: var(--h4-font-size-mobile, min(1.25rem, 5vw)) !important;
+        line-height: var(--h4-line-height-mobile, 125%) !important;
+        }
+      }
     
     `
   }
