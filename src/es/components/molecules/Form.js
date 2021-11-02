de7c0c437e06e4d208b6c1bcdc462074977ad8e3
joeljxse <joeljosewattwil@gmail.com>
2021-11-02 14:37:23 +0100

    m4musicform

---
+++ src/es/components/molecules/Form.js
@@ -0,0 +1,25 @@
+import BaseForm from '../web-components-cms-template/src/es/components/molecules/Form.js'
+
+export default class Form extends BaseForm {
+  constructor (...args) {
+    super(...args)
+  }
+
+  connectedCallback () {
+    if (this.shouldComponentRenderCSS()) this.renderCSS()
+    this.renderHTML()
+  }
+
+  shouldComponentRenderCSS () {
+    return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
+  }
+
+  renderCSS () {
+    this.css = /* css */`
+      :host a-input[type='radio'] { 
+        background-color: red;
+      }
+    
+    `
+  }
+}
