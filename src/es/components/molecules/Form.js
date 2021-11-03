52d0e5ae481c9a9aa76301b90b2ffd7e34f391b7
ed <88322685+edmgb@users.noreply.github.com>
2021-11-03 10:59:15 +0100

    fix: form radio buttons

--- src/es/components/molecules/Form.js
+++ src/es/components/molecules/Form.js
@@ -1,20 +1,19 @@
+// @ts-check
 import BaseForm from '../web-components-cms-template/src/es/components/molecules/Form.js'
 
-export default class Form extends BaseForm {
-  constructor(...args) {
-    super(...args)
-  }
+/* global self */
 
-  connectedCallback() {
+export default class Form extends BaseForm {
+  connectedCallback () {
     if (this.shouldComponentRenderCSS()) this.renderCSS()
     this.renderHTML()
   }
 
-  shouldComponentRenderCSS() {
+  shouldComponentRenderCSS () {
     return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
   }
 
-  renderCSS() {
+  renderCSS () {
     super.renderCSS()
     this.css = /* css */`
       :host fieldset {
@@ -26,15 +25,22 @@ export default class Form extends BaseForm {
         line-height: var(--h4-line-height, 125%) !important;
       }
 
-      :host .show-error{
+      :host .error-message {
         font-size:0.5rem;
-        color:#FF7373;
+        color:var(--field-error-color, #FF7373);
         padding:var(--field-padding-error, 0);
         display: flex;
         flex-direction: row;
         align-items: center;
         align-content: center;
+      }
+
+      :host .show-error {
         display:visible;
+      }
+
+      :host .hide-error{
+        display:none;
       }
 
       :host .description {
@@ -43,11 +49,25 @@ export default class Form extends BaseForm {
         padding:var(--field-description-padding, 0);
       }
 
+      :host .radio-group{
+        display:flex;
+        flex-direction:row;
+        align-items:flex-start;
+        width:var(--radio-group-width, auto);
+        justify-content: space-between;
+      }
+
+      :host [type=radio] {
+        margin:var(--radio-margin, 0);
+        align-self:center;
+      }
+
+
       :host select {
         /* display: inline-block;
         box-sizing: border-box;*/
         padding:var(--form-select-padding, 0);
-        border: 1px solid var(--form-select-border, white);
+        border:1px solid var(--form-select-border, white);
         font: inherit;
         line-height: inherit;
         -webkit-appearance: none;
@@ -61,16 +81,16 @@ export default class Form extends BaseForm {
         background-color:var(--form-select-background-color, white);
         color:var(--form-select-color, white);
         font-size:var(--form-select-font-size, inherit);
-        width: var(--form-select-width, auto);
+        width:var(--form-select-width, auto);
         margin:var(--form-select-margin, 0);
       }
 
       :host select:focus-visible {
         -webkit-appearance: none;
-        -moz-appearance: none;
-        -ms-appearance: none;
-        appearance: none;
-        border: 1px solid var(--form-select-border, white);
+        -moz-appearance:none;
+        -ms-appearance:none;
+        appearance:none;
+        border:1px solid var(--form-select-border, white);
       }
 
       :host .register-label{
@@ -80,13 +100,13 @@ export default class Form extends BaseForm {
       
       @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
         :host h4.form-caption {
-          font-size: var(--h4-font-size-mobile, min(1.25rem, 5vw)) !important;
-          line-height: var(--h4-line-height-mobile, 125%) !important;
+          font-size:var(--h4-font-size-mobile, min(1.25rem, 5vw)) !important;
+          line-height:var(--h4-line-height-mobile, 125%) !important;
         }
         :host select {
           padding:var(--form-select-padding-mobile, 0);
           font-size:var(--form-select-font-size-mobile, inherit);
-          width: var(--form-select-width-mobile, auto);
+          width:var(--form-select-width-mobile, auto);
           margin:var(--form-select-margin-mobile, 0);
         }
 
@@ -94,6 +114,16 @@ export default class Form extends BaseForm {
           font-size:var(--field-description-font-size-mobile, inherit);
           width:var(--field-description-width-mobile, auto);
         }
+
+        :host .radio-group{
+          width:100%;
+        }
+
+        :host [type=radio] {
+          align-items:center;
+          margin:var(--radio-margin-mobile, 0);
+        }
+        
       }
     
     `
