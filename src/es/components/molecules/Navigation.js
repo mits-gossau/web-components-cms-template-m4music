65e0c3e3577de57180f809e0bb5013f1f7bf90a9
ed <88322685+edmgb@users.noreply.github.com>
2021-11-22 08:13:28 +0100

    Merge branch 'master' into feature/navigation

--- src/es/components/molecules/Navigation.js
+++ src/es/components/molecules/Navigation.js
@@ -25,12 +25,58 @@ export default class Navigation extends BaseNavigation {
   renderCSS () {
     super.renderCSS()
     this.css = /* css */`
-      :host {
-        margin:30px;
+      :host > nav > ul {
+        background-color:var(--background-color, white);
+        flex-wrap:var(--flex-wrap, unset);
+      }
+      :host > nav > ul > li {
+        margin:0 0.5rem 0 0;
+      }
+      :host .icons {
+        visibility:hidden;
+      }
+      
+      :host > nav > ul li > a-link:hover {
+        background-color:#434343;
+      }
+
+      :host > nav > ul li.open > a-link {
+        background-color:#434343;
       }
       @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {  
-        :host {
-          margin:30px;
+        
+        :host > nav > ul {
+          background-color:var(--background-color-mobile, white);
+          width:auto;
+        }
+
+        :host > nav > ul > li {
+          margin:0 0 3rem 0;
+        }
+
+        :host > nav > ul li a-link.open ~ ul {
+          background-color:var(--background-color-child-mobile, white);
+        }
+
+        :host > nav > ul li.open > a-link {
+          background-color:inherit;
+        }
+
+        :host > nav > ul li.open  {
+          color:white;
+          
+        }
+        :host > nav > ul li > a-link:hover {
+          background-color:inherit;
+        }
+
+        :host .icons {
+          visibility:visible;
+          display:flex;
+          /* TODO: Remove! */
+          -webkit-filter: invert(100%);
+          filter: invert(100%);
+          /* ============= */
         }
       }
     `
