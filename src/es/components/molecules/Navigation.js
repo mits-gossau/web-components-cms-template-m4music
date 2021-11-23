944bd3de4b67306afc2f9164ca451ddad0e6029f
edmgb <evangelos.dinopoulos@mgb.ch>
2021-11-23 10:33:10 +0100

    feat: navigation styles

--- src/es/components/molecules/Navigation.js
+++ src/es/components/molecules/Navigation.js
@@ -4,9 +4,9 @@ import BaseNavigation from '../web-components-cms-template/src/es/components/mol
 /* global self */
 
 export default class Navigation extends BaseNavigation {
-  
 
-  renderCSS () {
+
+  renderCSS() {
     super.renderCSS()
     this.css = /* css */`
       :host > nav > ul {
@@ -16,46 +16,43 @@ export default class Navigation extends BaseNavigation {
       :host > nav > ul > li {
         margin:0 0.5rem 0 0;
       }
-      :host .icons {
-        visibility:hidden;
-      }
       
       :host > nav > ul li > a-link:hover {
         background-color:#434343;
       }
-
       :host > nav > ul li.open > a-link {
         background-color:#434343;
       }
+      :host > nav > ul > li.icon-list{
+        display:none;
+      }
       @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {  
-        
         :host > nav > ul {
           background-color:var(--background-color-mobile, white);
           width:auto;
         }
-
         :host > nav > ul > li {
           margin:0 0 3rem 0;
         }
-
         :host > nav > ul li a-link.open ~ ul {
           background-color:var(--background-color-child-mobile, white);
         }
-
         :host > nav > ul li.open > a-link {
           background-color:inherit;
         }
-
         :host > nav > ul li.open  {
+          color:white; 
+        }
+        :host > nav > ul > li  a:hover{
           color:white;
-          
         }
         :host > nav > ul li > a-link:hover {
           background-color:inherit;
         }
-
+        :host > nav > ul > li.icon-list{
+          display:inherit;
+        }
         :host .icons {
-          visibility:visible;
           display:flex;
           /* TODO: Remove! */
           -webkit-filter: invert(100%);
