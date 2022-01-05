26896fbce0549edfa1c46509543f127b561770c8
edmgb <evangelos.dinopoulos@mgb.ch>
2022-01-05 08:53:51 +0100

    fix: same navigation for mobile and desktop

--- src/es/components/molecules/Navigation.js
+++ src/es/components/molecules/Navigation.js
@@ -7,69 +7,30 @@ export default class Navigation extends BaseNavigation {
   renderCSS () {
     super.renderCSS()
     this.css = /* css */ `
-      
-      :host > nav > ul li > a-link:hover {
-        background-color:#434343;
-      }
-      :host > nav > ul li.open > a-link {
-        background-color:#434343;
-      }
-      :host > nav > ul > li.icon-list{
-        display:block;
-      }
       :host .icons {
         display:flex;
-      }
-      :host > nav > ul > li.meta-nav{
-        /*display: flex;
-        background: transparent;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+        padding:0.5rem 0 0.5rem 0;
         border-top: 1px solid black;
         border-bottom: 1px solid black;
-        line-height: 100%;
-        flex-direction: column;
-        align-items: center;*/
       }
-      :host  > nav > ul > li.meta-nav > ul a {
-        font-size:10px;
+      :host .meta {
+        padding:0.5rem 0 0 0;
+        border-top: 1px solid black;
+      }
+      :host .icons > a-logo {
+        margin:0 1rem 0 0;
+      }
+      :host .icons > a-logo:last-of-type {
+        margin:0;
+      }
+      :host(.no-scroll) > nav > ul li.meta ul a-link {
+        --header-navigation-font-size:min(24px, max(16px, 0.8rem));
       }
       @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {  
-        :host {
-          --color-hover: white;
-        }
-        :host > nav > ul {
-          background-color:var(--background-color-mobile, white);
-          width:auto;
-        }
-        :host > nav > ul > li {
-          margin:0 0 1.8rem 0;
-        }
-        :host > nav > ul li a-link.open ~ ul {
-          background-color:var(--background-color-child-mobile, white);
-        }
-        :host > nav > ul li.open > a-link {
-          background-color:inherit;
-        }
-        :host > nav > ul li > a-link:hover {
-          background-color:inherit;
-        }
-        :host > nav > ul > li.icon-list{
-          display:inherit;
-        }
-        :host .icons {
-          display:flex;
-        }
-        /*:host > nav > ul > li.meta-nav > ul{
-          display:block;
-          background:transparent;
-          border-top:1px solid black;
-          border-bottom:1px solid black;
-          width:50%;
-          line-height:100%;
-        }
-        :host  > nav > ul > li.meta-nav > ul a {
-          font-size:10px;
-        }*/
-        
       }
     `
   }
