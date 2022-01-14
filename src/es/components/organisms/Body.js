2cbbeea8984d038f6d6b8c4a2b35ad20ad63d5b7
joeljxse <joeljosewattwil@gmail.com>
2022-01-14 01:35:41 +0100

    bsvc

--- src/es/components/organisms/Body.js
+++ src/es/components/organisms/Body.js
@@ -16,6 +16,10 @@ import BaseBody from '../web-components-cms-template/src/es/components/organisms
  * }
  */
 export default class Body extends BaseBody {
+  constructor (...args) {
+    super({ mode: self.isBSVC ? 'false' : 'true' }, ...args) // disabling shadow-DOM on BSVC page to allow crowdsignal embed to work
+  }
+
   renderCSS () {
     super.renderCSS()
     this.css = /* css */ `
@@ -64,41 +68,7 @@ export default class Body extends BaseBody {
     :host .detail-image{
       width:100%;
     }
-
-    :host .embed-container .youtube-player {
-      width: 50vw;
-      height: 30vw;
-    }
-    :host .embed-container .css-answer-group.pds-answer-group {
-      margin: 20px 0 40px 0;
-    }
-    :host .embed-container .css-answer-row.pds-answer-row {
-      margin-top: 15px;
-    }
-    :host .embed-container .css-radiobutton.pds-radiobutton {
-      transform: translateY(-5px);
-      margin-right: 10px;
-    }
-    :host .embed-container .css-question-top.pds-question-top {
-      font-size: var(--h2-font-size);
-    }
-    :host .embed-container .css-votebutton-outer.pds-votebutton-outer a {
-      font-size: var(--button-font-size-mobile);
-      padding: 0.5rem 0.75rem;
-      background-color: var(--color-orange);
-      color: white;
-      margin-bottom: 15px;
-      cursor: pointer;
-      display: inline-block;
-      text-decoration: none;
-      border-radius: 4px;
-      transition: 0.3s all;
-    }
-    :host .embed-container .css-votebutton-outer.pds-votebutton-outer a:hover,
-    :host .embed-container .css-votebutton-outer.pds-votebutton-outer a:focus,
-    :host .embed-container .css-votebutton-outer.pds-votebutton-outer a:active { 
-      background-color: #E53C1A;
-    }
+    
     @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
       :host {
         background-color:var(--background-color-mobile, inherit);
@@ -117,16 +87,6 @@ export default class Body extends BaseBody {
       }
       :host ul {
         margin-top: 0;
-      }
-
-      :host .embed-container .youtube-player {
-        width: 80vw;
-        height: 50vw;
-        margin: 0 auto;
-      }
-      :host .embed-container .css-radiobutton.pds-radiobutton {
-        transform: translateY(-2px);
-        margin-right: 5px;
       }
     }
     `
